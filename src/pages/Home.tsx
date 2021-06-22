069bfd77eb3157ba2d2ed274fda577610415482b
caiohenrique-developer <caiorussellrox@msn.com>
2021-06-22 00:20:16 -0300

    :recycle: [ REFACTOR ]: Create useAuth hook ctx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
-import { useContext } from "react";
 import { useHistory } from "react-router-dom";
-import { AuthContext } from "../App";
+import { useAuth } from "../hooks/useAuth";
 
 import { Button } from "../components/Button";
 
@@ -12,7 +11,7 @@ import "../styles/auth.scss";
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext);
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
     if (!user) {
