04027a50aac2b5e816cb580ea8a4c40add813f27
caiohenrique-developer <caiorussellrox@msn.com>
2021-06-25 13:44:34 -0300

    :recycle: [ REFACTOR ]: Create AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { AuthContextProvider } from "./hooks/useAuth";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
